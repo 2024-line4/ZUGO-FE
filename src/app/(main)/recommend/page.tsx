1cae938e6f7a983790ec7a6fb4922d53c1fd79be
xeonsim <xeonsim@gmail.com>
2024-11-15 22:56:47 +0900

    add recommend list

--- src/app/(main)/recommend/page.tsx
+++ src/app/(main)/recommend/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import style from "./recommend.module.scss";
 import Filter from "./_components/filter";
+import RecommendList from "./_components/RecommendList";
 
 export const metadata: Metadata = {
   title: "Recommend",
@@ -10,8 +11,25 @@ export const metadata: Metadata = {
 export default function Recommend() {
   return (
     <>
-      <section className={style.searchSection}></section>
+      <section className={style.searchSection}>
+        <div className="grid grid-cols-3 place-content-center justify-items-center">
+          <div className="grid grid-cols-1">
+            <div>profile Img</div>
+            <div>user name</div>
+          </div>
+          <div className="grid grid-cols-1">
+            <div>출신 학교 :</div>
+            <div>학과 :</div>
+            <div>학년 :</div>
+            <div>선호 국가 :</div>
+          </div>
+          <div className="grid grid-cols-1">
+            <div>AI recommend</div>
+          </div>
+        </div>
+      </section>
       <Filter />
+      <RecommendList />
       <section></section>
     </>
   );
--- src/hooks/queries.ts
+++ src/hooks/queries.ts
@@ -1,6 +1,7 @@
 import { DormitoryCardType } from "@/types/DormitoryType";
 import { SchoolCardType } from "@/types/SchoolType";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { RecommendCardType } from "@/types/RecommendType";
 
 type GetDataProps = {
   country: string;
@@ -78,6 +79,66 @@ export function useGetDormitory({ country, region }: GetDataProps) {
       }
 
       return lastPageParam + 1;
+    },
+  });
+}
+
+type GetDataPropsRecommend = {
+  country: string;
+  region: string;
+  major: string;
+  univ: string;
+};
+
+// 추천 정보 받아오는 쿼리문
+
+type GetRecommendResponseType = Promise<{
+  schools: SchoolCardType[];
+}>;
+
+export function useGetRecommend({
+  country,
+  region,
+  major,
+  univ,
+}: GetDataPropsRecommend) {
+  return useInfiniteQuery({
+    queryKey: ["schools", country, region, region, univ],
+    queryFn: async ({
+      pageParam,
+    }: {
+      pageParam: number;
+    }): GetRecommendResponseType => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/info?country=${country}&region=${region}&major=${major}&univ=${univ}`,
+      );
+      console.log(pageParam);
+      // console.log(await response.json());
+
+      const recommendlist = await response.json();
+      if (!response.ok) {
+        throw new Error("GET RECOMMEND ERROR");
+      }
+      const res = await Promise.all(
+        recommendlist.schools.map(async (el) => {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BASE_URL}/univ?school=${el.id}`,
+          );
+          const r = await res.json();
+          r.id = el.id;
+          return r;
+        }),
+      );
+      console.log(res);
+
+      return res;
+    },
+    gcTime: 300 * 1000,
+    staleTime: 60 * 1000,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, _, lastPageParam) => {
+      console.log(lastPage, lastPageParam);
+      return 1;
     },
   });
 }
